ec969e167b07c7b15bc1b0e1076b64690d2074fa
josue5032 <rojopi@hotmail.es>
2021-11-24 16:25:54 -0500

    modelo

---
+++ src/main/java/modelo/Order.java
@@ -0,0 +1,52 @@
+package modelo;
+
+import java.sql.Date;
+import java.util.ArrayList;
+
+public class Order {
+	private int id;
+	private Date date;
+	private Client client;
+	private ArrayList<Product> products;
+	private ArrayList<Service> services;
+	public Order() {
+		
+	}
+	public Order(Date date, Client client, ArrayList<Product> products, ArrayList<Service> services) {
+		this.date = date;
+		this.client = client;
+		this.products = products;
+		this.services = services;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	public Client getClient() {
+		return client;
+	}
+	public void setClient(Client client) {
+		this.client = client;
+	}
+	public ArrayList<Product> getProducts() {
+		return products;
+	}
+	public void setProducts(ArrayList<Product> products) {
+		this.products = products;
+	}
+	public ArrayList<Service> getServices() {
+		return services;
+	}
+	public void setServices(ArrayList<Service> services) {
+		this.services = services;
+	}
+	
+}	
