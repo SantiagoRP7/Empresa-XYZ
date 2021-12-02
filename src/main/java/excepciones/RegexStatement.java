25da87ef89c4c4138333c6eee9c4d7803c48e5b1
Gabriel Salazar <gabrielsameth@gmail.com>
2021-12-01 22:34:37 -0500

    cambio en el regexstatement, se comenta el de contraseña

--- src/main/java/excepciones/RegexStatement.java
+++ src/main/java/excepciones/RegexStatement.java
@@ -5,5 +5,5 @@ public class RegexStatement {
     public static final String RegexUserName="^[a-zA-Z0-9_.]+$";
     public static final String RegexEmail="^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*" +
             "@" + "(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
-    public static final String RegexContrasena="^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,16}$";
+    //public static final String RegexContrasena="^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,16}$";
 }
